d14e0265a76a21e3f254555ef28f71bdc9954f5d
JAMES KURIA CHEGE <jameskuria04@gmail.com>
2025-09-10 00:59:35 +0300

    Add maneno.me domain to list of allowed domains on vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+	return {
+		plugins: [react()],
+		publicDir: "public",
+		optimizeDeps: {
+			exclude: ["lucide-react"],
+		},
+		server: {
+			host: "::",
+			// port: 8047,
+			proxy: {
+				"/api": {
+					target: "http://localhost:8000",
+					changeOrigin: true,
+					secure: false,
+				},
+			},
+		},
+		preview: {
+			port: 80, // Ensure this matches your desired port
+			host: "0.0.0.0", // Allow external access (needed for domain access)
+			allowedHosts: ["maneno.me", "www.maneno.me"], // Add your domain here
+		},
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
+		},
+	};
+});
